783a2d1187e7ea8215f0f0facab553c293cf24fd
Janhavi Jagtap <jagtapjanhavi2000@gmail.com>
2025-03-31 13:40:34 +1100

    Initial Commit

---
+++ BubbleTrouble/BubbleTroubleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleTroubleApp.swift
+//  BubbleTrouble
+//
+//  Created by Janhavi Jagtap on 31/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct BubbleTroubleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BubbleTroubleTests/BubbleTroubleTests.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleTroubleTests.swift
+//  BubbleTroubleTests
+//
+//  Created by Janhavi Jagtap on 31/3/2025.
+//
+
+import Testing
+@testable import BubbleTrouble
+
+struct BubbleTroubleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
