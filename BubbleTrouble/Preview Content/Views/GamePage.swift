9a105f18c897be3d5e4bd15c76531e46224a7b85
Janhavi Jagtap <janhavijagtap@Janhavis-MacBook-Air.local>
2025-04-21 23:52:50 +1000

    initial project

---
+++ BubbleTrouble/Preview Content/Views/GamePage.swift
@@ -0,0 +1,18 @@
+//
+//  GamePage.swift
+//  BubbleTrouble
+//
+//  Created by Janhavi Jagtap on 31/3/2025.
+//
+
+import SwiftUI
+
+struct GamePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GamePage()
+}
